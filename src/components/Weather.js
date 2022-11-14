1b665ed333f7a82f71503bd5083aeda901e18bc3
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2022-11-14 10:46:41 +0200

    components add + API setup

---
+++ src/components/Weather.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const url =
+  "https://api.openweathermap.org/data/2.5/weather?q=dallas&units=imperial&appid=ed33c4a398883e8b658dba1c86ce84e9";
+
+const Weather = () => {
+  const [data, setData] = useState(null);
+
+  return <div></div>;
+};
+
+export default Weather;
